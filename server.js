1587bf1eba9563a7e84ce318b83997667ead5d35
goforward-z <zlepeng.ucf@gmail.com>
2022-08-14 14:03:22 -0400

    updated server

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+// helper function
+const helpers = require('./utils/helpers');
 
-
-
+// handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
 
 // session (connects session to sequelize Database)
 const session = require('express-session');
@@ -17,21 +20,23 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // create session 
 const sess = {
-    secret: "super super secret",
-    cookie: { originalMaxAge: 600000 },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  
-  app.use(session(sess));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  secret: "super super secret",
+  cookie: { originalMaxAge: 600000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
+// set Handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
