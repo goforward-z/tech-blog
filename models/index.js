4662bbae72d0b3c8807867add5073e8f61374f71
goforward-z <zlepeng.ucf@gmail.com>
2022-08-14 12:30:03 -0400

    set up index in models

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const Comment = require('./Comment');
+
+// users can make many posts 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+}); 
+
+// a post can only belong to one user 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// a comment can only belong to one user 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// a comment can only belong to one user 
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// users can make many comments 
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+// users can make many posts 
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { Comment };
