5395a2c0e2e0953e70c0eb19df23a83419c548ea
goforward-z <zlepeng.ucf@gmail.com>
2022-08-15 10:59:53 -0400

    fixed post issue

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,7 +6,7 @@ const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
-router.use('./posts',postRoutes);
+router.use('/posts',postRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
